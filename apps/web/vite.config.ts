c55b40f1a64e0a3736fd2635eca59804d3d7f29b
Roi Susi <65774200+roisusi@users.noreply.github.com>
2026-08-18 23:19:13 +0300

    configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@api': fileURLToPath(new URL('./src/api', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+      '@providers': fileURLToPath(new URL('./src/providers', import.meta.url)),
+      '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
+      '@theme': fileURLToPath(new URL('./src/theme', import.meta.url)),
+    },
+  },
 })
